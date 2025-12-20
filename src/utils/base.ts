ac438cf95a1fd2ab5961aec5f1093b7c0698fb5e
lazhamei <115733150+lazhamei@users.noreply.github.com>
2025-12-21 02:21:30 +0800

    修复咸鱼大冲关刷新时间

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -1,8 +1,15 @@
 import moment from "moment";
 
 
-// 判断当前时间是否在本周内
+// 判断当前时间是否在本周内（周一00点重置）
 export const isInCurrentWeek = (timestamp: number, weekStart = 1) => {
+  // 设置周一为一周的开始
+  moment.locale('zh-cn', {
+    week: {
+      dow: 1, // 周一为一周的第一天
+      doy: 4
+    }
+  });
   const t = moment(timestamp);
   const today = moment();
   return t.isSame(today, 'week');
